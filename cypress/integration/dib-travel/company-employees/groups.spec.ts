e5583c68dbf77f2bb04be02b2799a93cc7f0cf71
Dusan Petrovic <dusanp93@live.com>
2021-06-29 11:04:08 +0200

    change beforeEach

--- cypress/integration/dib-travel/company-employees/groups.spec.ts
+++ cypress/integration/dib-travel/company-employees/groups.spec.ts
@@ -9,10 +9,13 @@ describe('Company Employees - Groups Page', () => {
     });
   });
 
-  it('should allow regular user to add new group', () => {
+  beforeEach(() => {
     cy.login();
     cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
+    cy.get('[href="/people-management/groups"]').click();
+  });
+
+  it('should allow regular user to add new group', () => {
     cy.get('dib-people-management dib-groups ui-button button').contains('Add Group').click();
     cy.get('.cdk-overlay-container dib-group-dialog input[placeholder="group name*"]').type(groupsDetails.name);
     cy.get('.cdk-overlay-container dib-group-dialog dib-assign-members .members mat-checkbox').click();
@@ -21,13 +24,16 @@ describe('Company Employees - Groups Page', () => {
   });
 
   // TODO make better wait
-  it('should allow regular user to edit created group', () => {
-    cy.login();
-    cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
+  it.only('should allow regular user to edit created group', () => {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(3000);
-    cy.get('dib-groups .body').find('dib-expandable-item').first().find('ui-button button').contains('edit').click();
+    cy.get('dib-expandable-item h2')
+      .contains('asdf')
+      .parent('dib-expandable-item')
+      .within(() => {
+        return cy.get('ui-button button').contains('edit').click();
+      });
+    //cy.get('dib-groups .body').find('dib-expandable-item').first().find('ui-button button').contains('edit').click();
 
     // cy.get('dib-people-management dib-groups dib-expandable-item ui-button button').contains('edit').click();
     cy.get('.cdk-overlay-container dib-group-dialog input[placeholder="group name*"]')
@@ -39,46 +45,6 @@ describe('Company Employees - Groups Page', () => {
 
   // TODO make better wait
   it('should allow regular user to delete created group', () => {
-    cy.login();
-    cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(3000);
-    cy.get('dib-people-management dib-groups dib-expandable-item ui-button button').contains('delete').click();
-    cy.get('.cdk-overlay-container confirmation-dialog ui-button button').contains('Delete').click();
-  });
-
-  it('should allow agent to add new group', () => {
-    cy.loginAgent();
-    cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
-    cy.get('dib-people-management dib-groups ui-button button').contains('Add Group').click();
-    cy.get('.cdk-overlay-container dib-group-dialog input[placeholder="group name*"]').type(groupsDetails.name);
-    cy.get('.cdk-overlay-container dib-group-dialog dib-assign-members .members mat-checkbox').click();
-    cy.get('.cdk-overlay-container dib-group-dialog ui-button button').contains('save').click();
-    cy.get('dib-people-management dib-groups .body').should('contain', groupsDetails.name);
-  });
-
-  // TODO make better wait
-  it('should allow agent to edit created group', () => {
-    cy.loginAgent();
-    cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000);
-    cy.get('dib-people-management dib-groups dib-expandable-item ui-button button').contains('edit').click();
-    cy.get('.cdk-overlay-container dib-group-dialog input[placeholder="group name*"]')
-      .clear()
-      .type(groupsDetails.editName);
-    cy.get('.cdk-overlay-container dib-group-dialog ui-button button').contains('save').click();
-    cy.get('dib-people-management dib-groups .body').should('contain', groupsDetails.editName);
-  });
-
-  // TODO make better wait
-  it('should allow regular user to delete created group', () => {
-    cy.loginAgent();
-    cy.get('dib-layout dib-hamburger-icon').click();
-    cy.get('[routerLink="/people-management/groups"]').click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(3000);
     cy.get('dib-people-management dib-groups dib-expandable-item ui-button button').contains('delete').click();
