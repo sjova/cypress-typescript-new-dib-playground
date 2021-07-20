aac1097207dd21e1c8c250f30a8e383cd9bb98e8
Dusan Petrovic <dusanp93@live.com>
2021-07-20 11:58:26 +0200

    edit selectors, remove .only from test

--- cypress/integration/dib-travel/sign-in/sign-in.spec.ts
+++ cypress/integration/dib-travel/sign-in/sign-in.spec.ts
@@ -13,15 +13,15 @@ describe('Sign in Page', () => {
   });
 
   it('should check agent redirection link', () => {
-    cy.get('a[href="/login/agent"]').should('have.text', 'Agent login');
+    cy.get('new-login a[href="/login/agent"]').should('have.text', 'Agent login');
   });
 
   it('should check forgot-password redirection link', () => {
-    cy.get('a[href="/forgot-password"]').should('have.text', 'Forgot password?');
+    cy.get('new-login .forgot-password-wrapper a[href="/forgot-password"]').should('have.text', 'Forgot password?');
   });
 
   it('should check sign-up redirection link', () => {
-    cy.get('a[href="/sign-up"]').should('have.text', "Don't have an account?");
+    cy.get('new-login a[href="/sign-up"]').should('have.text', "Don't have an account?");
   });
 
   it('should display error message when empty form is submitted', () => {
@@ -34,11 +34,11 @@ describe('Sign in Page', () => {
   it('should display error message when wrong password is submitted', () => {
     cy.get('new-login ui-input input[name=email]').type(loginDetails.invalidAccount.email);
     cy.get('new-login ui-input input[name=password]').type(loginDetails.invalidAccount.password);
-    cy.get('new-login button').click();
+    cy.get('new-login ui-button button').click();
     cy.get('.cdk-overlay-container simple-snack-bar > span').should('contain', 'Invalid Credentials');
   });
 
-  it.only('should allow user to sign in with valid credentials', () => {
+  it('should allow user to sign in with valid credentials', () => {
     cy.get('new-login ui-input input[name=email]').clear().type(loginDetails.defaultAccount.email);
     cy.get('new-login ui-input input[name=password]').clear().type(loginDetails.defaultAccount.password);
     cy.get('new-login ui-button button').click();
