458ce0532b29f461d7b6d1d8634e5c0eee8ffb0e
Dusan Petrovic <dusanp93@live.com>
2021-07-21 16:46:56 +0200

    add test when empty email input is submitted

--- cypress/integration/dib-travel/forgot-password/forgot-password.spec.ts
+++ cypress/integration/dib-travel/forgot-password/forgot-password.spec.ts
@@ -1,4 +1,8 @@
+import { DibTravelAccounts } from '../../../models';
+
 describe('Forgot Password Page', () => {
+  let accountEmail: DibTravelAccounts;
+
   beforeEach(() => {
     cy.visit('/');
     cy.get('new-login a[href="/forgot-password"]').click();
@@ -8,5 +12,10 @@ describe('Forgot Password Page', () => {
     cy.get('new-forgot-password span').should('contain', 'Forgot password');
     cy.get('new-forgot-password input[type="email"]').should('be.visible');
     cy.get('new-forgot-password ui-button button').should('contain', 'Send');
+  });
+
+  it.only('should display error message when empty email input field is submitted', () => {
+    cy.get('new-forgot-password ui-button button').click();
+    cy.get('new-forgot-password .error').should('contain', 'Email is required.');
   });
 });
