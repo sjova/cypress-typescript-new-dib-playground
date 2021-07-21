b9fe0a0faab47e57627b7d9a213efaa582b31302
Dusan Petrovic <dusanp93@live.com>
2021-07-21 18:20:57 +0200

    add invalid email address test

--- cypress/integration/dib-travel/sign-up/sign-up.spec.ts
+++ cypress/integration/dib-travel/sign-up/sign-up.spec.ts
@@ -18,7 +18,7 @@ describe('Sign-up Page', () => {
     cy.visit('/sign-up');
   });
 
-  it.only('should display error messages when empty sign-up form is submitted', () => {
+  it('should display error messages when empty sign-up form is submitted', () => {
     cy.get('dib-signup ui-button button').contains('Sign up').click();
     cy.get('dib-signup ui-input .error')
       .should('contain', 'First name is required')
@@ -29,6 +29,28 @@ describe('Sign-up Page', () => {
       .should('contain', 'Company registration number is required');
     cy.get('dib-signup ui-phone-picker .error').should('contain', 'Phone number is required.');
     cy.get('dib-signup ui-autocomplete .error').should('contain', 'Country is required');
+  });
+
+  it('should display error message when invalid email address is inserted', () => {
+    const email = userSignUpUpEmailPassword.defaultAccount.email;
+    const invalidEmail = email.replace('@', '');
+    cy.get('dib-signup ui-input input[name="firstName"]').type(registrationUserDetails.firstName);
+    cy.get('dib-signup ui-input input[name="lastName"]').type(registrationUserDetails.lastName);
+    cy.get('dib-signup ui-input input[name="email"]').type(invalidEmail);
+    cy.get('dib-signup ui-phone-picker ui-autocomplete').click();
+    cy.get('.cdk-overlay-container ui-panel cdk-virtual-scroll-viewport')
+      .scrollTo(0, 8000)
+      .contains(registrationUserDetails.companyCountry)
+      .click();
+    cy.get('dib-signup ui-input input[type="number"]').type(registrationUserDetails.phoneNumber);
+    cy.get('dib-signup ui-input input[name="password"]').eq(0).type(userSignUpUpEmailPassword.signUpAccount.password);
+    cy.get('dib-signup ui-input input[name="password"]').eq(1).type(userSignUpUpEmailPassword.signUpAccount.password);
+    cy.get('dib-signup ui-input input[name="companyName"]').type(registrationUserDetails.companyName);
+    cy.get('dib-signup ui-input input[name="companyRegistrationNumber"]').type(
+      registrationUserDetails.companyRegistrationNumber
+    );
+    cy.get('dib-signup ui-button button').contains('Sign up').click();
+    cy.get('dib-signup ui-input .error').should('contain', 'The email should be in email@example.com format');
   });
 
   it('should display successful message after user signs up with valid data', () => {
