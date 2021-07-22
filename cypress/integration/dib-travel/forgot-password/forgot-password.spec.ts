8e9f5d214c4acb0f685fab9e938b0849e6e7418b
Dusan Petrovic <dusanp93@live.com>
2021-07-22 15:14:08 +0200

    add back login redirection test

--- cypress/integration/dib-travel/forgot-password/forgot-password.spec.ts
+++ cypress/integration/dib-travel/forgot-password/forgot-password.spec.ts
@@ -21,6 +21,10 @@ describe('Forgot Password', () => {
     cy.get('new-forgot-password ui-button button').should('contain', 'Send');
   });
 
+  it('should check back login redirection link', () => {
+    cy.get('new-forgot-password a[href="/login"]').should('contain', 'Back');
+  });
+
   it('should display error message when empty email input field is submitted', () => {
     cy.get('new-forgot-password ui-button button').click();
 
