aebc6cadbcc537441468ebc8bdbf5428ef939c1c
Stefan Nikolic Vivant <stefan.nikolic@vivant.eco>
2021-07-23 13:11:22 +0200

    add intercept to travel document test

--- cypress/models/index.ts
+++ cypress/models/index.ts
@@ -1,5 +1,6 @@
 export * from './company-employees/employee';
 export * from './company-employees/group';
 export * from './dib-travel-accounts';
+export * from './profile/profile';
 export * from './sign-up/user';
 export * from './user';
