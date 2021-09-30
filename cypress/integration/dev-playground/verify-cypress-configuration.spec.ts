3641641c1c32a67c277356c4cbac5bc1fba9609c
Sasa Jovanovic <jovanovic.v.sasa@gmail.com>
2021-09-30 11:08:12 +0200

    update cypress and dep libs, update readme, few small fixes and cleanup

--- cypress/integration/dev-playground/verify-cypress-configuration.spec.ts
+++ cypress/integration/dev-playground/verify-cypress-configuration.spec.ts
@@ -3,8 +3,8 @@ describe('Cypress.config()', () => {
     const myConfig = Cypress.config();
 
     expect(myConfig).to.have.property('animationDistanceThreshold', 5);
-    expect(myConfig).to.have.property('defaultCommandTimeout', 4000);
-    expect(myConfig).to.have.property('requestTimeout', 5000);
+    expect(myConfig).to.have.property('defaultCommandTimeout', 16000);
+    expect(myConfig).to.have.property('requestTimeout', 15000);
     expect(myConfig).to.have.property('responseTimeout', 30000);
     expect(myConfig).to.have.property('pageLoadTimeout', 60000);
 
@@ -12,7 +12,6 @@ describe('Cypress.config()', () => {
     expect(myConfig).to.have.property('baseUrl', 'https://develop--dib-travel.netlify.app/');
     expect(Cypress.config('pluginsFile').toString().includes('cypress/plugins/index.ts')).to.be.true;
     expect(Cypress.config('supportFile').toString().includes('cypress/support/index.ts')).to.be.true;
-    expect(myConfig).to.have.property('testFiles', '**/*.spec.ts');
     expect(myConfig).to.have.property('video', false);
     expect(myConfig).to.have.property('viewportHeight', 720);
     expect(myConfig).to.have.property('viewportWidth', 1280);
