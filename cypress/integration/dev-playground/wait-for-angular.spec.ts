9b068a815396e1099074fe0686e7ef0e6880c7ab
Sasa Jovanovic <jovanovic.v.sasa@gmail.com>
2022-01-28 09:54:41 +0100

    few quick updates

--- cypress/integration/dev-playground/wait-for-angular.spec.ts
+++ cypress/integration/dev-playground/wait-for-angular.spec.ts
@@ -25,10 +25,11 @@ describe('Wait For Angular Until It Becomes Ready', () => {
   describe('Element Existence', () => {
     beforeEach(() => {
       cy.visit('/login');
-      cy.waitForAngular();
     });
 
     it('should demonstrate conditional testing when an element exists', () => {
+      cy.waitForAngular();
+
       cy.get('new-login').then(($newLogin) => {
         const primaryButton = $newLogin.find('ui-button[type="primary"] button');
 
@@ -42,6 +43,8 @@ describe('Wait For Angular Until It Becomes Ready', () => {
     });
 
     it('should demonstrate conditional testing when an element not exists', () => {
+      cy.waitForAngular();
+
       cy.get('new-login').then(($newLogin) => {
         const secondaryButton = $newLogin.find('ui-button[type="secondary"] button');
 
@@ -54,4 +57,41 @@ describe('Wait For Angular Until It Becomes Ready', () => {
       });
     });
   });
+
+  /*
+  describe('Element Existence Additional Example', () => {
+    beforeEach(() => {
+      cy.login();
+      cy.visitAngularUrl('/people-management/groups');
+    });
+
+    it('should demonstrate conditional testing in the groups listing', () => {
+      // Reference: https://docs.cypress.io/guides/core-concepts/conditional-testing#Element-existence
+
+      // Please create this group manually to confirm conditional testing
+      const testGroupName = 'Element Existence Demo Group';
+
+      // `waitForAngular` must be used before conditional testing (otherwise, it will not work)
+      cy.waitForAngular();
+
+      // Start checking from parent feature `dib-people-management dib-groups`
+      cy.get('dib-people-management dib-groups').then(($groups) => {
+        // This was set only for demo purposes (you don't need to separate this in real test case)
+        const contentWrapperSelector = 'dib-page .body .items';
+
+        if ($groups.find(`${contentWrapperSelector} div[dib-empty-list-label]`).length) {
+          cy.wrap($groups)
+            .find(`${contentWrapperSelector} div[dib-empty-list-label]`)
+            .contains('You have not yet created any groups.')
+            .should('exist');
+        } else {
+          cy.wrap($groups)
+            .find(`${contentWrapperSelector} dib-expandable-item .collapsed .item__main h2`)
+            .contains(testGroupName)
+            .should('exist');
+        }
+      });
+    });
+  });
+  */
 });
